b1f2ee226d2a7c47167c9e4eb28b76290b54148c
Ayfri <pierre.ayfri@gmail.com>
2020-10-31 16:42:12 +0100

    Removed unused code, updated libs.

--- deps.ts
+++ deps.ts
@@ -1,11 +1,11 @@
-export {sendMessage} from 'https://x.nest.land/Discordeno@9.0.1/src/handlers/channel.ts';
-export type {Message} from 'https://x.nest.land/Discordeno@9.0.1/src/structures/message.ts';
-export {Intents} from 'https://x.nest.land/Discordeno@9.0.1/src/types/options.ts';
-export {Collection} from 'https://x.nest.land/Discordeno@9.0.1/src/utils/collection.ts';
+export {sendMessage} from 'https://x.nest.land/Discordeno@9.0.15/src/handlers/channel.ts';
+export type {Message} from 'https://x.nest.land/Discordeno@9.0.15/src/structures/message.ts';
+export {Intents} from 'https://x.nest.land/Discordeno@9.0.15/src/types/options.ts';
+export {Collection} from 'https://x.nest.land/Discordeno@9.0.15/src/utils/collection.ts';
 export {SEP} from 'https://deno.land/std/path/mod.ts';
 export {exec} from 'https://cdn.depjs.com/exec/mod.ts';
 
-import Client, {botID} from 'https://x.nest.land/Discordeno@9.0.1/src/module/client.ts';
+import Client, {botID} from 'https://x.nest.land/Discordeno@9.0.15/src/module/client.ts';
 
 export {botID};
 export default Client;
--- mod.ts
+++ mod.ts
@@ -1,25 +1,11 @@
-import {createRequire} from 'https://deno.land/std@0.74.0/node/module.ts';
 import {configs} from './configs.ts';
 import Client, {botID, Collection, Intents, Message, sendMessage, SEP} from './deps.ts';
 import type Command from './src/classes/Command.ts';
 
 
 export const commands = new Collection<string, Command>();
-/*commands.set(compileCommand.name, compileCommand);
-console.log(`Command '${compileCommand.name}' loaded.`);
-commands.set(reloadCommand.name, reloadCommand);
-console.log(`Command '${reloadCommand.name}' loaded.`);*/
-
-/*const require = createRequire(import.meta.url);
-for(let command of [...cmdDir]) {
-	if (command.isFile && command.name.endsWith('.ts')) {
-		const path: string = Deno.realPathSync(configs.commandsFolder + SEP + command.name);
-		console.log(path);
-		const cmdClass = require(path).default;
-		console.log(cmdClass);
-	}
-}*/
 const cmdDir: Iterable<Deno.DirEntry> = Deno.readDirSync(Deno.realPathSync(configs.commandsFolder));
+
 for (let command of [...cmdDir]) {
 	if (command.isFile && command.name.endsWith('.ts')) {
 		const path: string = Deno.realPathSync(configs.commandsFolder + SEP + command.name);
