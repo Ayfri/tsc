60f5d4c59e83b2804a4aeefd7491b88015412b95
Ayfri <pierre.ayfri@gmail.com>
2020-11-03 14:58:59 +0100

    Added '.dcignore'.

--- src/commandsManager.ts
+++ src/commandsManager.ts
@@ -1,5 +1,4 @@
-import {configs} from './configs.ts';
-import {Collection, SEP} from './deps.ts';
+import {Collection, dotEnvConfig, SEP} from './deps.ts';
 import type Command from './src/classes/Command.ts';
 
 export const commands = new Collection<string, Command>();
@@ -7,7 +6,7 @@ export const commands = new Collection<string, Command>();
 export async function commandsManager(cmdDir: Iterable<Deno.DirEntry>): Promise<void> {
 	for (let command of [...cmdDir]) {
 		if (command.isFile && command.name.endsWith('.ts')) {
-			const path: string = Deno.realPathSync(configs.commandsFolder + SEP + command.name);
+			const path: string = Deno.realPathSync(dotEnvConfig.commandsFolder + SEP + command.name);
 			const commandClass: any = await import(`file://${path}`);
 			const cmd: any = commandClass.default;
 			if (!cmd) {
