36a1fa6570a98510b1760512884938c8105542e5
Ayfri <pierre.ayfri@gmail.com>
2021-01-20 20:58:09 +0100

    Readded ReloadCommand.

--- mod.ts
+++ mod.ts
@@ -1,48 +1,48 @@
-import {Discord, dotEnvConfig, Message} from './deps.ts';
-import type Command from './src/classes/Command.ts';
-import {commands, commandsManager} from './src/commandsManager.ts';
+import { commands, commandsManager } from "./src/commandsManager.ts";
+import { Discord, dotEnvConfig, Message } from "./deps.ts";
+import type Command from "./src/classes/Command.ts";
 
 const cmdDir: Iterable<Deno.DirEntry> = Deno.readDirSync(
-	Deno.realPathSync(dotEnvConfig.commandsFolder),
+  Deno.realPathSync(dotEnvConfig.commandsFolder),
 );
 await commandsManager(cmdDir);
 
 const a = {
-	lol: {
-		non: 'oui',
-	},
+  lol: {
+    non: "oui",
+  },
 };
 
-const {lol: {non}} = a;
+const { lol: { non } } = a;
 
 export default Discord.startBot({
-	token:         dotEnvConfig.token,
-	intents:       [
-		Discord.Intents.GUILD_MESSAGES,
-		Discord.Intents.GUILDS,
-		Discord.Intents.DIRECT_MESSAGES,
-	],
-	eventHandlers: {
-		ready() {
-			return console.info('Ready ! ');
-		},
-		async messageCreate(message: Message) {
-			if (message.author.bot) return;
-			if (new RegExp(`<@!?${Discord.botID}>`).test(message.content)) {
-				return message.reply('Use the bot with the `tsc ` prefix !');
-			}
-			
-			const args: string[] = message.content.slice(dotEnvConfig.prefix.length)
-				.trim().split(/\s+?/);
-			const cmd: Command | undefined = commands.find((c: Command) =>
-				c.name.toLowerCase().includes(args[0].toLowerCase()),
-			);
-			
-			if (message.content.startsWith(dotEnvConfig.prefix) && cmd) {
-				args.shift();
-				console.log(`Command '${cmd.name}' executed.`);
-				await cmd.run(message, args);
-			}
-		},
-	},
+  token: dotEnvConfig.token,
+  intents: [
+    Discord.Intents.GUILD_MESSAGES,
+    Discord.Intents.GUILDS,
+    Discord.Intents.DIRECT_MESSAGES,
+  ],
+  eventHandlers: {
+    ready() {
+      return console.info("Ready ! ");
+    },
+    async messageCreate(message: Message) {
+      if (message.author.bot) return;
+      if (new RegExp(`<@!?${Discord.botID}>`).test(message.content)) {
+        return message.reply("Use the bot with the `tsc ` prefix !");
+      }
+
+      const args: string[] = message.content.slice(dotEnvConfig.prefix.length)
+        .trim().split(/\s+?/);
+      const cmd: Command | undefined = commands.find((c: Command) =>
+        c.name.toLowerCase().includes(args[0].toLowerCase())
+      );
+
+      if (message.content.startsWith(dotEnvConfig.prefix) && cmd) {
+        args.shift();
+        console.log(`Command '${cmd.name}' executed.`);
+        await cmd.run(message, args);
+      }
+    },
+  },
 });
--- src/commands/EvalCommand.ts
+++ src/commands/EvalCommand.ts
@@ -1,58 +1,59 @@
-import {Message} from '../../deps.ts';
-import Command from '../classes/Command.ts';
-import {codeBlock, compileTSToJS, crop, isOwner} from '../utils/utils.ts';
+import { Message } from "../../deps.ts";
+import { Discord } from "../../deps.ts";
+import Command from "../classes/Command.ts";
+import { codeBlock, compileTSToJS, crop, isOwner } from "../utils/utils.ts";
 
 export default class EvalCommand extends Command {
-	public constructor() {
-		super('eval');
-	}
-	
-	public async run(message: Message, args: string[]): Promise<void> {
-		function send(
-			content: string,
-			channelID: string = message.channelID,
-		): Promise<any> {
-			return message.send(crop(content, 2000));
-		}
-		
-		function sendJS(
-			content: string,
-			channelID: string = message.channelID,
-		): Promise<any> {
-			return sendMarkdown(content, 'ts', channelID);
-		}
-		
-		function sendMarkdown(
-			content: string,
-			language: string = 'ts',
-			channelID: string = message.channelID,
-		): Promise<any> {
-			return send(codeBlock(content, language), channelID);
-		}
-		
-		if (!isOwner(message.author.id)) {
-			await send('Blah, t\'as pas la permission !');
-		} else {
-			try {
-				async function wait(callback: () => any): Promise<void> {
-					try {
-						await callback();
-					} catch (e) {
-						await sendJS(decodeURI(Deno.inspect(e, {
-							sorted: true,
-						})));
-					}
-				}
-				
-				const code = args.join(' ')
-					.replace(/```(?:[a-z0-9]{1,12})?([\S\s]*)```/g, '$1');
-				
-				await eval(await compileTSToJS(`wait(async function(){${code}})`));
-			} catch (e) {
-				await sendJS(decodeURI(Deno.inspect(e, {
-					sorted: true,
-				})));
-			}
-		}
-	}
+  public constructor() {
+    super("eval");
+  }
+
+  public async run(message: Message, args: string[]): Promise<void> {
+    function send(
+      content: string,
+      channelID: string = message.channelID,
+    ): Promise<any> {
+      return message.send(crop(content, 2000));
+    }
+
+    function sendJS(
+      content: string,
+      channelID: string = message.channelID,
+    ): Promise<any> {
+      return sendMarkdown(content, "ts", channelID);
+    }
+
+    function sendMarkdown(
+      content: string,
+      language: string = "ts",
+      channelID: string = message.channelID,
+    ): Promise<any> {
+      return send(codeBlock(content, language), channelID);
+    }
+
+    if (!isOwner(message.author.id)) {
+      await send("Blah, t'as pas la permission !");
+    } else {
+      try {
+        async function wait(callback: () => any): Promise<void> {
+          try {
+            await callback();
+          } catch (e) {
+            await sendJS(decodeURI(Deno.inspect(e, {
+              sorted: true,
+            })));
+          }
+        }
+
+        const code = args.join(" ")
+          .replace(/```(?:[a-z0-9]{1,12})?([\S\s]*)```/g, "$1");
+
+        await eval(await compileTSToJS(`wait(async function(){${code}})`));
+      } catch (e) {
+        await sendJS(decodeURI(Deno.inspect(e, {
+          sorted: true,
+        })));
+      }
+    }
+  }
 }
