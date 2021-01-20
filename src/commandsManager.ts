af35dcfc520f52092d142c01f7bd9413cc0f8241
Ayfri <pierre.ayfri@gmail.com>
2021-01-20 21:42:21 +0100

    Refactored commandsManager.

--- src/commandsManager.ts
+++ src/commandsManager.ts
@@ -6,19 +6,19 @@ export const commands = new Collection<string, Command>();
 export async function commandsManager(
 	cmdDir: Iterable<Deno.DirEntry>,
 ): Promise<void> {
-	for (let command of [...cmdDir]) {
-		if (command.isFile && command.name.endsWith('.ts')) {
+	for (let commandFile of [...cmdDir]) {
+		if (commandFile.isFile && commandFile.name.endsWith('.ts')) {
 			const path: string = Deno.realPathSync(
-				dotEnvConfig.commandsFolder + SEP + command.name,
+				dotEnvConfig.commandsFolder + SEP + commandFile.name,
 			);
-			const commandClass: any = await import(`file://${path}`);
-			const cmd: any = commandClass.default;
-			if (!cmd) {
-				console.warn(`Could not load '${command.name}' command !`);
+			const imported = await import(`file:///${path}`);
+			const command = new (imported.default)();
+			if (!command) {
+				console.warn(`Could not load '${commandFile.name}' command !`);
 				continue;
 			}
-			commands.set(cmd.name, new cmd());
-			console.log(`Successfully loaded '${cmd.name}' command !`);
+			commands.set(commandFile.name, command);
+			console.log(`Successfully loaded '${command.name}' command !`);
 		}
 	}
 }
