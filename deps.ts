e1257319d0169a48818dc048d9c1bff2693972c0
Ayfri <pierre.ayfri@gmail.com>
2021-01-22 00:14:04 +0100

    Fixed formatting for deps.

--- deps.ts
+++ deps.ts
@@ -1,9 +1,8 @@
-import { config } from "https://deno.land/x/dotenv/mod.ts";
+import {config} from 'https://deno.land/x/dotenv/mod.ts';
 
-export { SEP } from "https://deno.land/std/path/mod.ts";
-export { exec } from "https://cdn.depjs.com/exec/mod.ts";
-export * as Discord from "https://deno.land/x/discordeno@10.0.2/mod.ts";
-export { Collection } from "https://deno.land/x/discordeno@10.0.2/mod.ts";
-export type { Message } from "https://deno.land/x/discordeno@10.0.2/mod.ts";
+export {SEP} from 'https://deno.land/std@0.84.0/path/mod.ts';
+export * as Discord from 'https://deno.land/x/discordeno@10.0.2/mod.ts';
+export {Collection} from 'https://deno.land/x/discordeno@10.0.2/mod.ts';
+export type {Message} from 'https://deno.land/x/discordeno@10.0.2/mod.ts';
 
 export const dotEnvConfig = config();
