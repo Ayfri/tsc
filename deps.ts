13c4fdf7a2b34794b947529c2e7740b38087828a
Ayfri <pierre.ayfri@gmail.com>
2021-03-06 18:19:31 +0100

    chore(deps): Update dependencies.

--- deps.ts
+++ deps.ts
@@ -1,8 +1,8 @@
 import {config} from 'https://deno.land/x/dotenv/mod.ts';
 
-export {SEP} from 'https://deno.land/std@0.84.0/path/mod.ts';
-export * as Discord from 'https://deno.land/x/discordeno@10.0.2/mod.ts';
-export {Collection} from 'https://deno.land/x/discordeno@10.0.2/mod.ts';
-export type {Message} from 'https://deno.land/x/discordeno@10.0.2/mod.ts';
+export {SEP} from 'https://deno.land/std@0.89.0/path/mod.ts';
+export * as Discord from 'https://deno.land/x/discordeno@10.4.0/mod.ts';
+export {Collection} from 'https://deno.land/x/discordeno@10.4.0/mod.ts';
+export type {Message} from 'https://deno.land/x/discordeno@10.4.0/mod.ts';
 
 export const dotEnvConfig = config();
