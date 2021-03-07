8799f64b2e83cac9011207d0195a891ba4816353
Ayfri <pierre.ayfri@gmail.com>
2021-03-07 02:25:02 +0100

    refactor: Removed duplicates.

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -10,18 +10,3 @@ export function crop(text: string, maxLength?: number): string {
 export function commandToFile(command: string): string {
 	return `${command[0].toUpperCase()}${command.slice(1)}Command.ts`;
 }
-
-export async function compileTSToJS(code: string) {
-	const {files} = await Deno.emit('/assets/temp.ts', {
-		sources:         {
-			'/assets/temp.ts': code,
-		},
-		compilerOptions: {
-			noImplicitAny: false,
-			sourceMap:     false,
-			declaration:   false,
-		},
-	});
-
-	return Object.values(files)[0];
-}
