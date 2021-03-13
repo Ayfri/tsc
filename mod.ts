bff228bd5559b43b6bec04c5d205146d2da293fd
Ayfri <pierre.ayfri@gmail.com>
2021-03-13 15:41:51 +0100

    fix: Use .env config for owners into mod.ts.

--- mod.ts
+++ mod.ts
@@ -2,13 +2,14 @@ import {CommandClient, dotEnvConfig, GatewayIntents, SEP} from './deps.ts';
 
 export const client = new CommandClient({
 	prefix: 'tsc',
-	owners: ['386893236498857985'],
+	owners: dotEnvConfig.owners.split(','),
 	allowBots: false,
 	token: dotEnvConfig.token,
 	intents: [
 		GatewayIntents.GUILD_MESSAGES,
 		GatewayIntents.GUILDS,
 		GatewayIntents.DIRECT_MESSAGES,
+		GatewayIntents.GUILD_EMOJIS
 	],
 });
 
