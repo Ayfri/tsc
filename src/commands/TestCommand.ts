f92f1c4665d56e096ffaf23dde043e908cc313a3
Ayfri <pierre.ayfri@gmail.com>
2021-03-13 15:01:19 +0100

    feat: Put test command into 'dev' category.

--- src/commands/TestCommand.ts
+++ src/commands/TestCommand.ts
@@ -1,8 +1,10 @@
 import {Command, CommandContext} from '../../deps.ts';
+import {LABS_TS, WAIT_EMOJI} from '../constants.ts';
 
 export default class TestCommand extends Command {
 	public name = 'test';
 	public aliases = ['t'];
+	public category = 'dev';
 
 	public async execute(ctx: CommandContext): Promise<void> {
 		ctx.message.reply('this is a test');
