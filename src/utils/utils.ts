60f180f726b6c280399d18de06edf6a97a0e4812
Ayfri <pierre.ayfri@gmail.com>
2021-03-24 18:31:50 +0100

    fix: Remove error.category in fancyFormatDiagnostics function.

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -15,7 +15,7 @@ export function fancyFormatDiagnostics(errors: Deno.Diagnostic[]): string {
 	return codeBlock(
 		crop(
 			errors
-				.map(e => `${e.category}${e.sourceLine}\n${' '.repeat(e.start?.character || e.end?.character || 0)}^\n${e.messageText}\nat line ${e.start?.line}:${e.start?.character}\nat line ${e.end?.line}:${e.end?.character}`)
+				.map(e => `${e.sourceLine}\n${' '.repeat(e.start?.character || e.end?.character || 0)}^\n${e.messageText}\nat line ${e.start?.line}:${e.start?.character}\nat line ${e.end?.line}:${e.end?.character}`)
 				.join('\n'),
 			1990),
 		'ts');
