7f0e5bf9ee3013b43154f5f6a72a43919e94b472
Ayfri <pierre.ayfri@gmail.com>
2021-03-24 17:41:23 +0100

    feat: Add fancyFormatDiagnostics function.

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -10,3 +10,13 @@ export function crop(text: string, maxLength?: number): string {
 export function commandToFile(command: string): string {
 	return `${command[0].toUpperCase()}${command.slice(1)}Command.ts`;
 }
+
+export function fancyFormatDiagnostics(errors: Deno.Diagnostic[]): string {
+	return codeBlock(
+		crop(
+			errors
+				.map(e => `${e.category}${e.sourceLine}\n${' '.repeat(e.start?.character || e.end?.character || 0)}^\n${e.messageText}\nat line ${e.start?.line}:${e.start?.character}\nat line ${e.end?.line}:${e.end?.character}`)
+				.join('\n'),
+			1990),
+		'ts');
+}
