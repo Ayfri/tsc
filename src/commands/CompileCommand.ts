bd87a2f176738da1aa08f3722af86b1c0aadec3d
Ayfri <pierre.ayfri@gmail.com>
2021-03-24 18:10:23 +0100

    feat: Add reactions to CompileCommand.

--- src/commands/CompileCommand.ts
+++ src/commands/CompileCommand.ts
@@ -1,10 +1,11 @@
 import {Command, CommandContext} from '../../deps.ts';
-import {codeBlock, crop} from '../utils/utils.ts';
+import {ERROR_EMOJI, WAIT_EMOJI} from '../constants.ts';
+import {codeBlock, crop, fancyFormatDiagnostics} from '../utils/utils.ts';
 
 export default class CompileCommand extends Command {
 	public name = 'compile';
 	public aliases = ['tsc', 'c'];
-	public category = 'utils';
+	public category = 'dev';
 
 	public static async compile(code: string) {
 		const result = await Deno.emit('/assets/temp.ts', {
@@ -34,6 +35,8 @@ export default class CompileCommand extends Command {
 	}
 
 	public async execute(ctx: CommandContext): Promise<void> {
+		await ctx.message.addReaction(WAIT_EMOJI);
+
 		const code: string = ctx.args.join(' ').replace(/```([tj]s|(type|java)script)?/gi, '').trim();
 		const {
 			resultCode,
@@ -41,18 +44,13 @@ export default class CompileCommand extends Command {
 			errors,
 		} = await CompileCommand.compile(code);
 
-		ctx.message.reply(codeBlock(crop(resultCode, 1990), 'js'));
-		if (types.length > 0) ctx.channel.send(`> **Types :**\n${codeBlock(crop(types, 1990), 'ts')}`);
+		await ctx.message.reply(codeBlock(crop(resultCode, 1990), 'js'));
+		if (types.trim().length > 0) ctx.channel.send(`> **Types :**\n${codeBlock(crop(types, 1990), 'ts')}`);
 		if (errors.length > 0) {
-			ctx.channel.send(`> **Errors :**\n${
-				codeBlock(
-					crop(
-						errors
-							.map(e => `${e.sourceLine}\n${' '.repeat(e.start?.character || e.end?.character || 0)}^\n${e.messageText}\nat line ${e.start?.line}:${e.start?.character}\nat line ${e.end?.line}:${e.end?.character}`)
-							.join('\n'),
-						1990),
-					'ts')}`);
+			ctx.message.addReaction(ERROR_EMOJI);
+			await ctx.channel.send(`> **Errors :**\n${(fancyFormatDiagnostics(errors))}`);
 		}
 
+		await ctx.message.removeReaction(WAIT_EMOJI, ctx.client.user);
 	}
 }
